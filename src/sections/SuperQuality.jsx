d2d887e09091fe001639bbdd0967c3f4313ced3b
Shahziab-code <shahzaib.soomro123@gmail.com>
2024-09-26 22:08:29 +0500

    initial commit

---
+++ src/sections/SuperQuality.jsx
@@ -0,0 +1,37 @@
+import Button from "../nike_landing_assets/Components/Button";
+import shoe8 from "../nike_landing_assets/assets/images/shoe8.svg";
+
+const SuperQuality = () => {
+  return (
+    <section
+      id="about-us"
+      className=" flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
+          </span>
+          <br />
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
+        </h2>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
+          Discover Stylish Nike arrivals, quality confort, and innovative for
+          your active life.
+        </p>
+        <Button label="View details" />
+      </div>
+      <div className="flex-1 flex justify-center items-center">
+        <img
+          src={shoe8}
+          alt="shoe8"
+          width={570}
+          height={522}
+          className="object-contain"
+        />
+      </div>
+    </section>
+  );
+};
+
+export default SuperQuality;
